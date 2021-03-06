d1999383b6e00d5dc7d9ed3e78951688ce6e920f
MHirano <michelle@hiranohomes.com>
2021-03-06 00:26:53 -0800

    questions 2-4

--- script.js
+++ script.js
@@ -5,12 +5,39 @@ var quizQuestions = [
         answers:{
             a:'short for varsity',
             b:'a name assigned to a literal valu eor object',
-            c:'a simple collection of similar object that can be accessed by a variable',
-            d:'a function that can loop'
+            c:'a simple collection of similar object that can be accessed by a variable'
+        }
+    }
+
+    {
+        question:'What syntax can you use to hold your Javascript in HTML?',
+        answers:{
+            a:'<src>',
+            b:'<script>',
+            c:'<java>'
+        }
+    }
+
+    {
+        question:'Which is not a valid JavaScript name?',
+        answers:{
+            a:'5dots',
+            b:'he_llo',
+            c:'javascriptQuiz'        
+        }
+    }
+
+    {
+        question:'Using ___ statement is how you test for a specific condition',
+        answers:{
+            a:'Select',
+            b:'For',
+            c:'If'
         }
     }
 
     {
         question:''
     }
+
 ]
